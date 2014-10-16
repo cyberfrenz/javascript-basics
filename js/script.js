56eb0efaaf83d2d98a19884591290e8bd3133012
Antony <antonz@uw.edu>
2014-10-16 10:09:07 -0700

    exploration

--- js/script.js
+++ js/script.js
@@ -5,3 +5,32 @@
 	Javascript 
 	Comment
 */
+"use strict";
+
+var person = {
+	fname: 'Antony',
+	lname: 'Zhong',
+	email: 'antonz@uw.edu',
+	getFullName: function() { return this.fName + ' ' + this.lname; } 
+};
+
+console.log(person.getFullName());
+
+
+for (var prop in person) {
+	console.log(person[prop]);
+}; 
+
+//create new img
+
+var elem = document.createElement('img');
+elem.src = 'img/dog.jpg';
+elem.alt = 'picture of something that i don';
+
+document.body.insertBefore(elem, document.body.firstChild);
+
+var elen = document.createElement('img');
+elen.src = 'img/cat.jpg';
+elen.alt = 'picture of something that i don';
+
+document.body.appendChild(elen);
